3f2246f35fbc8d14fee55fa5c870a1ca3e4cc443
= <breeroell@gmail.com>
2020-10-10 11:21:27 -0600

    updates

--- python/lib/DiceEngine.py
+++ python/lib/DiceEngine.py
@@ -8,9 +8,28 @@ class diceEngine(object):
 		"""
 		"""
 
-		self.att_roll_map = 
-			{
-				:
+		self.att_roll_map = {
+				9:{
+				"dice": 4,
+				"method": "high"
+				},
+				7:{
+				"dice": 3,
+				"method": "high"
+				},
+				5:{
+				"dice": 2,
+				"method": "high"
+				},
+				3:{
+				"dice": 3,
+				"method": "low"
+				},
+				1:{
+				"dice": 4,
+				"method": "low"
+				}
+
 			}
 		
 	def dice_roll(self, num_dice, die_size=8):
@@ -20,11 +39,39 @@ class diceEngine(object):
 		return: list of roll outcomes
 		"""
 		die_rolls = []
-		for die in num_dice:
+		for die in range(0, num_dice):
 			die_rolls.append(random.randrange(1, die_size))
 
 		return die_rolls
 		
+	def map_att_to_dice(self, attribute, skill, boon):
+		"""
+		"""
+
+		#calculate dice pool size and rolling method
+		pool_total = attribute + boon + skill
+
+		#use pool total to determine total dice and method
+		dice_to_roll = self.att_roll_map[pool_total]["dice"]
+		dice_method = self.att_roll_map[pool_total]["method"]
+
+		#roll dice and return a list
+		dice_results = self.dice_roll(num_dice=dice_to_roll)
+		print(dice_results)
+		#sort dice results for return
+		if dice_method == "low":
+			dice_results = sorted(dice_results)
+		elif dice_method == "high":
+			dice_results = sorted(dice_results, reverse=True)
+		
+		dice_sum = dice_results[0] + dice_results[1]
+
+		return {"result":dice_sum, "method": dice_method, 
+				"dice_results": dice_results, "pool_total":pool_total}
+		#returns the dice pool chosen as text and the result, 
+			#and the total including skill and boon bonuses
+		#maps a passed attribute using self.
+		#att_roll_map to determine which dice pool to use
 
 	def rollDice(self, attribute): #number range must be 1 to 5
 		if attribute == 1:
